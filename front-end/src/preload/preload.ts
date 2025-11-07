df9ab3dd37255e8e028e3d8bdae95bbcbbbd2e6b
Ilhami Kandemir <ilhami.1424@gmail.com>
2025-11-07 18:50:55 +0100

    architectural changes

--- front-end/src/preload/preload.ts
+++ front-end/src/preload/preload.ts
@@ -1,2 +1,5 @@
+// moved from front-end/preload/preload.ts
+
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
---
+++ front-end/src/renderer/preload.ts
@@ -0,0 +1,4 @@
+// moved from front-end/renderer/preload.ts
+// See the Electron documentation for details on how to use preload scripts:
+// https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
